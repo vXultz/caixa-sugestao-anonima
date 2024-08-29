c6eef0e4300bd30edc4a67bfc86a6edbf717533c
Vitor Schultz <vitorserta@gmail.com>
2024-08-29 17:30:05 -0300

    entidades criadas

---
+++ src/main/java/com/example/atividades/entity/ComentarioEntity.java
@@ -0,0 +1,24 @@
+package com.example.atividades.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "comentario")
+public class ComentarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sugestao_id")
+    private SugestaoEntity sugestaoId;
+
+    private String texto;
+
+    private LocalDateTime dataEnvio;
+}
---
+++ src/main/java/com/example/atividades/entity/SugestaoEntity.java
@@ -0,0 +1,24 @@
+package com.example.atividades.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "sugestao")
+public class SugestaoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String titulo;
+
+    private String descricao;
+
+    private LocalDateTime dataEnvio;
+
+    private LocalDateTime dataAtualizacao;
+}
